8da8809a94309f4524d139885193d2a7e1243605
Namit Rustagi <namitrustagi11@gmail.com>
2025-05-11 18:56:09 +0530

    Update available_models.py

--- available_models.py
+++ available_models.py
@@ -1,4 +1,4 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyAPNqBA-Y0JNVdAZLB5sZoU7r14ZY1hCqU")
+genai.configure(api_key="API_KEY")
 for model in genai.list_models():
     print(model.name, model.supported_generation_methods)
